9cf89e8fa1901af9b3a80bb5355e5a39b0d7a212
Alex McClung <alex.mcclung@hotmail.com>
2016-10-06 12:24:49 +1100

    tidy up blackjack demo

--- assessment_2/src/Blackjack.cpp
+++ assessment_2/src/Blackjack.cpp
@@ -15,15 +15,7 @@ Semester 2 2016
 using namespace std;
 using namespace alex_m;
 
-void display_output(HandOfCards* dealer, HandOfCards* player1)
-{
-  //Step 4: Display each hand’s content - for player and the dealer
-  cout << "Player: " << *player1 << "(" << player1->count() << " points)" << endl;
-  cout << "Dealer: " << *dealer << "(" << dealer->count() << " points)" << endl;
-}
-
 int main(int argc, char* argv[]) {
-
         
     if (argc >= 2) {
         //Initialise the random number generator by inputing a seed value as an argument 
@@ -36,147 +28,137 @@ int main(int argc, char* argv[]) {
         srand(seed);
     }
   
-  //Step 1: create a new instance of DeckOfCards storing a full deck of cards, shuffle
-  DeckOfCards* deck = new DeckOfCards();
-  deck->shuffle();
-  
-  //Step 2: Create two instances of HandOfCards, named player1 and dealer.
-  HandOfCards* player1 = new HandOfCards();
-  HandOfCards* dealer = new HandOfCards();
+    //Step 1: create a new instance of DeckOfCards storing a full deck of cards, shuffle
+    DeckOfCards deck;
+    deck.shuffle();
+    
+    //Step 2: Create two instances of HandOfCards, named player1 and dealer.
+    HandOfCards player1;
+    HandOfCards dealer;
 
-  //Step 3: Looping through the player and the dealer, give them one card each, facing up.
-  for (int i=0;i<2;i++)
-  {
-    Card card = deck->pop(); //Pop a card from the top of the deck
-
-    //Is this card going to the dealer or player (use modulo with two, to alternate with whether i is odd or even)
-    if (i%2 == 0)
+    //Step 3: Looping through the player and the dealer, give them one card each, facing up.
+    for (int i=0;i<2;i++)
     {
-      //Give Player the card from the top of the deck
-      player1->add(card, true);
+      Card card = deck.pop(); //Pop a card from the top of the deck
+
+      //Is this card going to the dealer or player (use modulo with two, to alternate with whether i is odd or even)
+      if (i%2 == 0)
+      {
+        //Give Player the card from the top of the deck
+        player1.add(card, true);
+      }
+      else
+      {
+        //Give Dealer a card from the top of the deck
+        dealer.add(card, true);
+      }
     }
-    else
+
+    //Then, give one card to player 1, facing up.
+    player1.add(deck.pop(), true);
+
+    //Then, give one card to the dealer, facing down.
+    dealer.add(deck.pop(), false);
+
+    //Step 4: Display each hand’s content - for player and the dealer
+    cout << "Player: " << player1 << "(" << player1.count() << " points)" << endl;
+    cout << "Dealer: " << dealer << "(" << dealer.count() << " points)" << endl;
+
+    //Step 5: Check if the player or the dealer have a count of 21
+    if (player1.count() == 21 && dealer.countAll() != 21)
     {
-      //Give Dealer a card from the top of the deck
-      dealer->add(card, true);
+      cout << "The player is the winner!" << endl;
+      return 0;
     }
-  }
+    else if ((player1.count() != 21 && dealer.countAll() == 21) || player1.count() > 21)
+    {
+      cout << "The dealer is the winner!" << endl;
+      return 0;
+    }
+    else if (player1.count() == 21 && dealer.countAll() == 21)
+    {
+      cout << "Tie! There are no winners." << endl;
+      return 0;
+    }
 
-  //Then, give one card to player 1, facing up.
-  player1->add(deck->pop(), true);
+    //Step 6: If that is not the case, then loop through the player while it has a hand <21
+    while (player1.count() < 21) {
+      cout << "Player, do you want to Hit (1) or Stand (2)?" << endl;
 
-  //Then, give one card to the dealer, facing down.
-  dealer->add(deck->pop(), false);
+      // Accept cin input
+      int input;
+      cin >> input;
 
-  //Step 4
-  display_output(dealer, player1);
+      // Prevent input of invalid characters, which will break the program (cause an infinite loop)
+      // Just ignore any invalid (non integer) input
+      if(cin.fail()) { cin.clear(); cin.ignore(); }
 
-  //Step 5: Check if the player or the dealer have a count of 21
-  if (player1->count() == 21 && dealer->countAll() != 21)
-  {
-    cout << "The player is the winner!" << endl;
-    delete deck;
-    delete player1;
-    delete dealer; //Clear the memory used on the heap
+      //If the answer is 1, then give the player another card from the deck, face-up, and go back to step (4).
+      if (input == 1)
+      {
+        player1.add(deck.pop(), true);
+
+        // Repeat Step 4: Display each hand’s content - for player and the dealer
+        cout << "Player: " << player1 << "(" << player1.count() << " points)" << endl;
+        cout << "Dealer: " << dealer << "(" << dealer.count() << " points)" << endl;
+        
+        // Repeat Step 5: Check if the player or the dealer have a count of 21
+        if (player1.count() == 21 && dealer.countAll() != 21)
+        {
+          cout << "The player is the winner!" << endl;
+          return 0;
+        }
+        else if ((player1.count() != 21 && dealer.countAll() == 21) || player1.count() > 21)
+        {
+          cout << "The dealer is the winner!" << endl;
+          return 0;
+        }
+        else if (player1.count() == 21 && dealer.countAll() == 21)
+        {
+          cout << "Tie! There are no winners." << endl;
+          return 0;
+        }
+      }
+      else if (input == 2)
+      {
+        //Step 7: Player is Stood, time to do the dealers play
+        while (dealer.countAll() < 17)
+        {
+          dealer.add(deck.pop(), true);
+        }
+        break;
+      }
+    }
+
+    //Step 8: Make all cards in the dealer’s hand face-up, and display the contents of both hands using
+    //the overloaded cout << operator, along with the count for each hand.
+    dealer.faceUp();
+    cout << "Player: " << player1 << "(" << player1.count() << " points)" << endl;
+    cout << "Dealer: " << dealer << "(" << dealer.count() << " points)" << endl;
+
+    //Step 9: Check who has the highest count among the two hands (but not over 21), and
+    // print “The player/dealer is the winner.” and exit. If both got exactly 21, then print
+    // “Tie! There are no winners.” 
+    if (player1.count() > 21)
+    {
+      cout << "The dealer is the winner!" << endl;
+    }
+    else if (dealer.count() > 21)
+    {
+      cout << "The player is the winner!" << endl;
+    }
+    else if (player1.count() > dealer.count())
+    {
+      cout << "The player is the winner!" << endl;    
+    }
+    else if (player1.count() == dealer.count())
+    {
+      cout << "Tie! There are no winners" << endl;    
+    }
+    else if (dealer.count() > player1.count())
+    {
+      cout << "The dealer is the winner!" << endl;    
+    }
+    
     return 0;
-  }
-  else if ((player1->count() != 21 && dealer->countAll() == 21) || player1->count() > 21)
-  {
-    cout << "The dealer is the winner!" << endl;
-    delete deck;
-    delete player1;
-    delete dealer; //Clear the memory used on the heap
-    return 0;
-  }
-  else if (player1->count() == 21 && dealer->countAll() == 21)
-  {
-    cout << "Tie! There are no winners." << endl;
-    delete deck;
-    delete player1;
-    delete dealer; //Clear the memory used on the heap
-    return 0;
-  }
-
-  //Step 6: If that is not the case, then loop through the player while it has a hand <21
-  while (player1->count() < 21) {
-    cout << "Player, do you want to Hit (1) or Stand (2)?" << endl;
-
-    // Accept cin input
-    int input;
-    cin >> input;
-
-    // Prevent input of invalid characters, which will break the program (cause an infinite loop)
-    // Just ignore any invalid (non integer) input
-    if(cin.fail()) { cin.clear(); cin.ignore(); }
-
-    //If the answer is 1, then give the player another card from the deck, face-up, and go back to step (4).
-    if (input == 1)
-    {
-      player1->add(deck->pop(), true);
-      //Return to steps 4 & 5
-      display_output(dealer, player1);
-      
-      //Step 5: Check if the player or the dealer have a count of 21
-      if (player1->count() == 21 && dealer->countAll() != 21)
-      {
-        cout << "The player is the winner!" << endl;
-        delete deck;
-        delete player1;
-        delete dealer; //Clear the memory used on the heap
-        return 0;
-      }
-      else if ((player1->count() != 21 && dealer->countAll() == 21) || player1->count() > 21)
-      {
-        cout << "The dealer is the winner!" << endl;
-        delete deck;
-        delete player1;
-        delete dealer; //Clear the memory used on the heap
-        return 0;
-      }
-      else if (player1->count() == 21 && dealer->countAll() == 21)
-      {
-        cout << "Tie! There are no winners." << endl;
-        delete deck;
-        delete player1;
-        delete dealer; //Clear the memory used on the heap
-        return 0;
-      }
-    }
-    else if (input == 2)
-    {
-      //Step 7: Player is Stood, time to do the dealers play
-      while (dealer->countAll() < 17)
-      {
-        dealer->add(deck->pop(), true);
-      }
-      break;
-    }
-  }
-
-  //Step 8: Make all cards in the dealer’s hand face-up, and display the contents of both hands using
-  //the overloaded cout << operator, along with the count for each hand.
-  dealer->faceUp();
-  cout << "Player: " << *player1 << "(" << player1->count() << " points)" << endl;
-  cout << "Dealer: " << *dealer << "(" << dealer->count() << " points)" << endl;
-
-  //Step 9: Check who has the highest count among the two hands (but not over 21), and
-  // print “The player/dealer is the winner.” and exit. If both got exactly 21, then print
-  // “Tie! There are no winners.” 
-  if (player1->count() > dealer->count())
-  {
-    cout << "The player is the winner!" << endl;    
-  }
-  else if (player1->count() == dealer->count())
-  {
-    cout << "Tie! There are no winners" << endl;    
-  }
-  else
-  {
-    cout << "The dealer is the winner!" << endl;    
-  }
-  
-  delete deck;
-  delete player1;
-  delete dealer; //Clear the memory used on the heap
-  return 0;
 }
